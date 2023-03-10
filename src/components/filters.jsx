e44fee23986939f8d814ca9ae52e0a4b5c38327a
Zach Justus <zach.justus1@gmail.com>
2023-03-10 19:21:40 +0000

    created filters plan

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -1,5 +1,11 @@
 export default function Filters(){
 
+    /**
+     * - [] Get Filters from API
+     * - [] display sliders / dropdowns
+     * - [] update proper streets
+     */
+
     return (
         <div>Hello WOrld</div>
     );
